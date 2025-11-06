a221ea2710b80f13b243d44ca67ae8a1fbc1158b
Yuichi Nakamura <y.512.nakamura@gmail.com>
2025-11-06 21:36:14 +0900

    Update elf2x68k version to 20251106

--- Formula/elf2x68k.rb
+++ Formula/elf2x68k.rb
@@ -2,7 +2,7 @@ class Elf2x68k < Formula
   desc "X68k cross development environment"
   homepage "https://github.com/yunkya2/elf2x68k"
   url "https://github.com/yunkya2/elf2x68k.git",
-     tag: "20250907"
+     tag: "20251106"
 
   depends_on "wget"
   depends_on "texinfo"
